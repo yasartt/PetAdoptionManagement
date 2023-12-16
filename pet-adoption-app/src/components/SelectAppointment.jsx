2d05ea516bfec8028526d7ad2235221d30b52f7e
Yaşar <yasar.tatlicioglu@ug.bilkent.edu.tr>
2023-12-16 13:09:16 +0300

    Değişiklikler

--- pet-adoption-app/src/components/SelectAppointment.jsx
+++ pet-adoption-app/src/components/SelectAppointment.jsx
@@ -3,7 +3,11 @@ import React, { useEffect, useState } from 'react';
 const SelectAppointment = ({ props }) => {
   const [seciliSeans, setSeciliSeans] = useState(0);
   const [seciliGun, setSeciliGun] = useState(0);
-  const [seciliHafta, setSeciliHafta] = useState(0);
+  const [seciliHafta, setSeciliHafta] = useState({
+    haftaNumarasi: 0, // You can set the initial week number as needed
+    baslangicTarihi: new Date('2023-12-11'), // Set the initial start date
+    bitisTarihi: new Date('2023-12-17'), // Set the initial end date
+  });
 
   const Weekdays = {
     0: 'Monday',
@@ -22,45 +26,47 @@ const SelectAppointment = ({ props }) => {
     3: '10:30-11:00',
     4: '11:00-11:30',
     5: '11:30-12:00',
-    6: '13:00-13:30',
-    7: '13:30-14:00',
-    8: '14:00-14:30',
-    9: '14:30-15:00',
-    10: '15:00-15:30',
-    11: '15:30-16:00',
-    12: '16:00-16:30',
-    13: '16:30-17:00',
-    14: '17:00-17:30',
-    15: '17:30-18:00',
-    16: '18:00-18:30',
-    17: '18:30-19:00',
-    18: '19:00-19:30',
-    19: '19:30-20:00',
+    6: '12:00-12:30',
+    7: '12:30-13:00',
+    8: '13:00-13:30',
+    9: '13:30-14:00',
+    10: '14:00-14:30',
+    11: '14:30-15:00',
+    12: '15:00-15:30',
+    13: '15:30-16:00',
+    14: '16:00-16:30',
+    15: '16:30-17:00',
+    16: '17:00-17:30',
+    17: '17:30-18:00',
+    18: '18:00-18:30',
+    19: '18:30-19:00',
+    20: '19:00-19:30',
+    21: '19:30-20:00',
   };
 
-  useEffect(() => {
-    // call to the api with veterinarian id to get the available appointment hours
+/**   useEffect(() => {
+    // call to the api with veterinarian id to get the available appointment hours with seciliGun and seciliSeans
 
-    console.log('seciliGun: ', seciliGun);
-    console.log('seciliSeans: ', seciliSeans);
-  }, []);
+  }, []);*/
 
   const calculateDateRange = (hafta) => {
     const today = new Date();
     const startDate = new Date(today);
     const endDate = new Date(today);
   
-    const startDayOfWeek = 1; // Monday
-    const endDayOfWeek = 1; // Sunday
   
-    const daysToAdd = startDayOfWeek - today.getDay() + (hafta * 7);
-    const daysToAddEnd = endDayOfWeek - today.getDay() + (hafta * 7) + 6;
+    const daysToAdd = 1 - today.getDay() + (hafta * 7);
+    const daysToAddEnd = 1 - today.getDay() + (hafta * 7) + 6;
   
     startDate.setDate(today.getDate() + daysToAdd);
     endDate.setDate(today.getDate() + daysToAddEnd);
   
     const startMonth = startDate.toLocaleString('en-US', { month: 'short' });
     const endMonth = endDate.toLocaleString('en-US', { month: 'short' });
+
+    
+    //setHaftaninIlkGunu(startDate);
+    //setHaftaninSonGunu(endDate);
   
     return `${startDate.getDate()} ${startMonth} - ${endDate.getDate()} ${endMonth}`;
   };
@@ -71,58 +77,96 @@ const SelectAppointment = ({ props }) => {
       <h3 style={{ fontFamily: 'Helvetica', fontSize: '13px', textAlign: 'center' }}>
         {Weekdays[gunIndex]}
       </h3>
-      {[...Array(19)].map((_, saatIndex) => (
+      {[...Array(22)].map((_, saatIndex) => (
         <button
           key={saatIndex}
           style={isItAvailable(gunIndex, saatIndex) ? buttonStyleAvailable : buttonStyleDisabled}
+          disabled={!isItAvailable(gunIndex, saatIndex)}
           onClick={() => handleSecim(gunIndex, saatIndex)}
         >
-          <p>{HourNames[saatIndex]}</p>
+           <p style={{ fontSize: 'small' }}>{HourNames[saatIndex]}</p>
         </button>
       ))}
     </div>
   );
 
   const isItAvailable = (gunIndex, saatIndex) => {
-    return true;
+
+    const today = new Date();
+    const selectedDate = new Date();
+
+    selectedDate.setDate(seciliHafta.baslangicTarihi.getDate() + gunIndex);
+
+    //selectedDate.setDate(today.getDate() + (gunIndex - today.getDay()) + seciliHafta.haftaNumarasi * 7);
+    const selectedHour = (Math.floor(saatIndex/2)) + 9; // Assuming hours start from 9:00 AM
+
+    // Compare the selected datetime with the current datetime
+    if (
+      selectedDate < today ||
+      (selectedDate.getDate() === today.getDate() && selectedHour <= today.getHours())
+    ) {
+      return false; // Time is in the future or the current hour
+    } else {
+      return true; // Time has passed
+    }
+
   };
 
   const handleSecim = (gunIndex, saatIndex) => {
-    console.log('gunIndex: ', gunIndex);
-    console.log('saatIndex: ', saatIndex);
+    //api call to send application request to the api, pet id, veterinarian id, selected date and time are needed
+    const gonderilecekTarih = new Date(seciliHafta.baslangicTarihi.getTime() + gunIndex * 24 * 60 * 60 * 1000);
+    
+    gonderilecekTarih.setHours(saatIndex + 9);
+    //console.log('Selected date:', seciliHafta.baslangicTarihi + gunIndex);
+    console.log('Gonderilecek Tarih:', gonderilecekTarih);
+
+
+
     setSeciliGun(gunIndex);
     setSeciliSeans(saatIndex);
   };
 
   const handleIncreaseWeek = () => {
-    setSeciliHafta((prevHafta) => prevHafta + 1);
+    setSeciliHafta(() => ({
+      haftaNumarasi: seciliHafta.haftaNumarasi + 1,
+      baslangicTarihi: new Date(seciliHafta.baslangicTarihi.getTime() + 7 * 24 * 60 * 60 * 1000),
+      bitisTarihi: new Date(seciliHafta.bitisTarihi.getTime() + 7 * 24 * 60 * 60 * 1000),
+    }));
+
+    console.log('Increased to:', seciliHafta);
   };
 
   const handleDecreaseWeek = () => {
-    setSeciliHafta((prevHafta) => Math.max(prevHafta - 1, 0));
-  };
+    setSeciliHafta(() => ({
+      
+      haftaNumarasi: seciliHafta.haftaNumarasi - 1,
+      baslangicTarihi: new Date(seciliHafta.baslangicTarihi.getTime() - 7 * 24 * 60 * 60 * 1000),
+      bitisTarihi: new Date(seciliHafta.bitisTarihi.getTime() - 7 * 24 * 60 * 60 * 1000),
+    }));
 
+    console.log('Decreased to:', seciliHafta);
+  };
   const buttonStyleAvailable = {
     backgroundColor: 'purple',
     color: 'white',
     padding: '10px',
     border: 'none',
     borderRadius: '5px',
-    width: '130px',
-    height: '40px',
+    width: '100px',
+    height: '30px',
     cursor: 'pointer',
     display: 'block',
     margin: '5px 0 0 0',
   };
 
   const buttonStyleDisabled = {
-    backgroundColor: 'black',
+    backgroundColor: 'grey',
     color: 'white',
     padding: '10px',
     border: 'none',
     borderRadius: '5px',
-    width: '130px',
-    height: '40px',
+    width: '100px',
+    height: '30px',
     cursor: 'pointer',
     display: 'block',
     margin: '5px 0 0 0',
@@ -133,7 +177,7 @@ const SelectAppointment = ({ props }) => {
       <h2 style={{ fontFamily: 'Helvetica', marginBottom: '10px' }}>Vet. Mehmet Dedeler</h2>
       <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px' }}>
         <button onClick={handleDecreaseWeek}>&#8592; Previous Week</button>
-        <h2 style={{ fontFamily: 'Helvetica' }}>{calculateDateRange(seciliHafta)}</h2>
+        <h2 style={{ fontFamily: 'Helvetica' }}>{calculateDateRange(seciliHafta.haftaNumarasi)}</h2>
         <button onClick={handleIncreaseWeek}>Next Week &#8594;</button>
       </div>
       <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
--- pet-adoption-app/src/pages/index.js
+++ pet-adoption-app/src/pages/index.js
@@ -4,8 +4,6 @@ import {Link } from "react-router-dom";
 function Home() {
   return (
     <div className="Home">
-        <div className='text-yellow-600'>sdabaksjd</div>
-        <p>DENEME CUM</p>
         <Link to="/auth">Auth</Link>
     </div>
   );
