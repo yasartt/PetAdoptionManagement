64587a4adb8f650f13eb49d01c9434a620c4cd31
mehmet dedeler <mehmet.dedeler@ug.bilkent.edu.tr>
2023-12-18 23:33:01 +0300

    fghfgn

--- pet-adoption-app/src/pages/auth.js
+++ pet-adoption-app/src/pages/auth.js
@@ -1,15 +1,70 @@
 import { useState,useEffect } from 'react';
 import logo from './../rabbit.png';
+import Register from './Register.jsx';
+import axios from 'axios'
 
 function Auth() {
+    const [formValues, setFormValues] = useState({
+        userType: '',
+        username: '',
+        password: ''
+      });
+      const handleInputChange = (e) => {
+        //const { name, value } = e.target;
+        setFormValues({
+          ...formValues,
+          [e.target.name]: e.target.value
+        });
+      };
+
+      // Handle form submit
+      const handleSubmit = (e) => {
+        e.preventDefault(); // Prevent default form submission behavior
+
+        // TODO: Implement what should happen on form submit
+        //console.log('Form submitted:', formValues);
+
+        //axios.post('https://localhost:7073/api/User/Loginuser', formValues).then(response => {});
+
+        localStorage.setItem('userType', formValues.userType);
+
+
+        // You might want to call an API to submit these values
+        // or dispatch an action if using Redux, for example.
+      };
     const [isLogin,setLogin] = useState(true);
     const [acc,setAcc] = useState(0)
 
-    const [addrtype, setAddrtype] = useState(["Adopter", "Shelter","Vet"])
+    const addrtype = ["Adopter", "Shelter", "Veterinarian"]
+
     const Add = addrtype.map(Add => Add
     )
     const handleAddrTypeChange = (e) => setAcc(e.target.value)
 
+
+//for login
+    const [accLogin,setAccLogin] = useState(0)
+
+    const addrtypeLogin = ["Adopter", "Shelter", "Veterinarian","Admin"]
+
+    const AddLogin = addrtypeLogin.map(AddLogin => AddLogin)
+
+    const handleAddrTypeChangeLogin = (e) => {
+        const newAccLogin = e.target.value;
+        setAccLogin(newAccLogin);
+        setFormValues({
+            ...formValues,
+            userType: addrtypeLogin[newAccLogin]
+        });
+    };
+//
+useEffect(() => {
+    setAccLogin(0)
+    setFormValues({
+        ...formValues,
+        userType: addrtypeLogin[0]
+    });
+},[])
     useEffect(() => {
         setAcc(0)
       }, [isLogin]);
@@ -28,40 +83,73 @@ function Auth() {
 
             </div>
 
-            
+            {!isLogin && (
             < select
                 onChange={e => handleAddrTypeChange(e)}
                 className="border-2 rounded-lg border-slate-800 p-1" >
                 {
                     Add.map((address, key) => <option value={key}>{address}</option>)
                 }
-            </select >
+            </select >)}
             
             {isLogin && (<p>Sign in to your account</p>)}
             {!isLogin && (<p>Sign up for {addrtype[acc]} account</p>)}
+            {isLogin && (
             <div className="flex flex-col justify-center space-y-3">
-                <form className="flex flex-col items-center">
+                <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+
+                    < select
+                        onChange={e => handleAddrTypeChangeLogin(e)}
+                        className="border-2 rounded-lg border-slate-800 p-1" >
+                        {
+                            AddLogin.map((address, key) => <option value={key}>{address}</option>)
+                        }
+                    </select >
+                    
                     <label className="self-start text-sm">Username:</label>
-                    <input  className="rounded-lg bg-bunny-100 p-1"  type="text"/> 
-                </form>
-                <form className="flex flex-col items-center">
+                    <input 
+                        className="rounded-lg bg-bunny-100 p-1"  
+                        type="text"
+                        name="username" // Added name attribute
+                        value={formValues.username} 
+                        onChange={handleInputChange}
+                    /> 
                     <label className="self-start text-sm">Password:</label>
-                    <input  className="rounded-lg bg-bunny-100 p-1" type="password"/> 
+                    <input  
+                        className="rounded-lg bg-bunny-100 p-1" 
+                        type="password"
+                        name="password" // Changed to password and added name attribute
+                        value={formValues.password} // Changed to password
+                        onChange={handleInputChange}
+                    /> 
+                    {isLogin ? (
+                        <button
+                            className="p-3 bg-bunny-400 rounded-2xl text-white cursor-pointer"
+                            type="submit" // Added type submit for proper form submission
+                        >
+                            Login
+                        </button>
+                    ) : (
+                        <button
+                            className="p-3 bg-bunny-400 rounded-2xl text-white cursor-pointer"
+                            type="submit" // Added type submit for proper form submission
+                        >
+                            Register
+                        </button>
+                    )}
                 </form>
 
             </div>
-            {isLogin && (
-            <div className="p-3 bg-bunny-400 rounded-2xl text-white">Login</div>
             )}
-            {!isLogin && (
-            <div className="p-3 bg-bunny-400 rounded-2xl text-white">Register</div>
-            )}
-            
+            {!isLogin &&(
+            <Register accIndex={acc}>
 
-
+            </Register>
+        )}
         </div>
-    </div>
-  );
+    </div>                    
+    )
+
 }
 
 export default Auth;
